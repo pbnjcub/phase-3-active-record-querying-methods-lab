2a7a29f67105301885489321639207c46683a36d
Paul Jung <pbnjcub@gmail.com>
2023-03-20 09:28:31 -0400

    commit tree ec17a174ba425ece2ebd72c7a91543f5c5030ec9

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,15 +2,15 @@ require "pry"
 
 class Show < ActiveRecord::Base
     def self.highest_rating
-        self.maximum("rating")
+        self.maximum(:rating)
     end
 
     def self.most_popular_show
-        self.order(:rating).last
+        self.order(rating: :desc).first
     end
 
     def self.lowest_rating
-        self.minimum("rating")
+        self.minimum(:rating)
     end
 
     def self.least_popular_show
@@ -22,12 +22,117 @@ class Show < ActiveRecord::Base
     end
 
     def self.popular_shows
-        self.where("rating > 5")
+        self.where('rating > 5')
     end
 
     def self.shows_by_alphabetical_order
-        self.order(name: :asc) 
+        self.order(name: :asc)
     end
 end
 
-# binding.pry
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------------------------------------------
+# class Show < ActiveRecord::Base
+#     def self.highest_rating
+#         self.maximum("rating")
+#     end
+
+#     def self.most_popular_show
+#         self.order(:rating).last
+#     end
+
+#     def self.lowest_rating
+#         self.minimum("rating")
+#     end
+
+#     def self.least_popular_show
+#         self.order(:rating).first
+#     end
+
+#     def self.ratings_sum
+#         self.sum(:rating)
+#     end
+
+#     def self.popular_shows
+#         self.where("rating > 5")
+#     end
+
+#     def self.shows_by_alphabetical_order
+#         self.order(name: :asc) 
+#     end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # class Show < ActiveRecord::Base
+# #     def self.highest_rating
+# #         self.maximum("rating")
+# #     end
+
+# #     def self.most_popular_show
+# #         self.order(:rating).last
+# #     end
+
+# #     def self.lowest_rating
+# #         self.minimum("rating")
+# #     end
+
+# #     def self.least_popular_show
+# #         self.order(:rating).first
+# #     end
+
+# #     def self.ratings_sum
+# #         self.sum(:rating)
+# #     end
+
+# #     def self.popular_shows
+# #         self.where("rating > 5")
+# #     end
+
+# #     def self.shows_by_alphabetical_order
+# #         self.order(name: :asc) 
+# #     end
+# # end
+
+# # # binding.pry
